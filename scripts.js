ac1d9bdc193f79c4987d587eed876466c02cd03a
SrTinny <joaovictorcardosoduartejoao@gmail.com>
2024-06-07 23:30:58 -0300

    commit

--- scripts.js
+++ scripts.js
@@ -10,41 +10,45 @@ document.addEventListener("DOMContentLoaded", function () {
   // Função para mostrar o slide atual
   function showSlide(index) {
     slides.forEach((slide, i) => {
-        slide.style.transform = `translateX(-${index * 100}%)`;
-        if (i === index) {
-            slide.classList.add('currentItem');
-        } else {
-            slide.classList.remove('currentItem');
-        }
+      slide.style.transform = `translateX(-${index * 100}%)`;
+      if (i === index) {
+        slide.classList.add("currentItem");
+      } else {
+        slide.classList.remove("currentItem");
+      }
     });
     console.log("Slide atual:", index);
-}
+  }
 
-function nextSlide() {
+  function nextSlide() {
     console.log("Clicou em próximo slide");
-    currentSlide = (currentSlide + 1) % slides.length;
+    if (currentSlide === slides.length - 1) {
+      // Se for o último slide, mover o primeiro slide para o final
+      carousel.appendChild(slides[0]);
+    } else {
+      currentSlide++;
+    }
     showSlide(currentSlide);
     console.log("Slide atual após nextSlide:", currentSlide);
-}
+  }
 
-
-
-function prevSlide() {
+  function prevSlide() {
     console.log("Clicou em slide anterior");
-    if (slides.length > 1) {
-        currentSlide = (currentSlide - 1 + slides.length) % slides.length;
-        showSlide(currentSlide);
+    if (currentSlide === 0) {
+      // Se for o primeiro slide, mover o último slide para o início
+      carousel.insertBefore(slides[slides.length - 1], slides[0]);
+    } else {
+      currentSlide--;
     }
+    showSlide(currentSlide);
     console.log("Slide atual após prevSlide:", currentSlide);
-}
-
+  }
 
   // Adiciona os ouvintes de eventos aos botões de seta
-  prevButton.addEventListener("click", function () {
-    prevSlide();
-  });
+  prevButton.addEventListener("click", prevSlide);
+  nextButton.addEventListener("click", nextSlide);
 
-  nextButton.addEventListener("click", function () {
-    nextSlide();
-  });
+  // Mostra o primeiro slide ao carregar a página
+  showSlide(currentSlide);
 });
+
