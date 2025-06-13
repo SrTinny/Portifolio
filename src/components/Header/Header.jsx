84249b1be590013c6606e2d94e323050773d622c
SrTinny <joaovictorcardosoduartejoao@gmail.com>
2025-06-13 10:19:03 -0300

    feat: botão themeToggle funcional

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,39 +1,34 @@
 // src/components/Header/Header.jsx
-import styles from "./Header.module.css";
-import logo from "../../assets/logo.jpg";
+import styles from "./Header.module.css"
+import logo from "../../assets/logo.jpg"
+import useThemeToggle from "../../hooks/useThemeToggle"
 
 export default function Header() {
+  const { isLightMode, toggleTheme } = useThemeToggle()
+
   return (
     <header className={styles.header}>
       <div className={styles.interface}>
         <a href="#" className={styles.logo}>
           <img
             src={logo}
-            alt="Logo da Tinny DEV"
+            alt="Logo"
             style={{ width: 60, height: 60, borderRadius: "50%" }}
           />
         </a>
 
         <nav className={`${styles.menuDesktop} fullscreen`}>
           <ul>
-            <li>
-              <a href="#home">Início</a>
-            </li>
-            <li>
-              <a href="#skills">Especialidades</a>
-            </li>
-            <li>
-              <a href="#about">Sobre</a>
-            </li>
-            <li>
-              <a href="#portfolio">Projetos</a>
-            </li>
+            <li><a href="#home">Início</a></li>
+            <li><a href="#skills">Especialidades</a></li>
+            <li><a href="#about">Sobre</a></li>
+            <li><a href="#portfolio">Projetos</a></li>
           </ul>
         </nav>
 
         <div className={styles.toggleTheme}>
-          <button className={styles.themeToggle}>
-            <i className="bi bi-brightness-high"></i>
+          <button className={styles.themeToggle} onClick={toggleTheme}>
+            <i className={`bi ${isLightMode ? "bi-moon-stars" : "bi-brightness-high"}`}></i>
           </button>
         </div>
 
@@ -48,5 +43,5 @@ export default function Header() {
         </div>
       </div>
     </header>
-  );
+  )
 }
